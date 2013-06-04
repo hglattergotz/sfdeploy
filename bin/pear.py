4c7336fbe1e82bd3d7d091429feda40932d73e67
Henning Glatter-Gotz <henning@glatter-gotz.com>
2013-06-04 17:55:51 -0400

    Add missing import for shell module

--- bin/pear.py
+++ bin/pear.py
@@ -7,12 +7,13 @@ A task to detect whether a specific PEAR package is installed or not
 import os
 from fabric.api import *
 from fabric.colors import red, green
+import shell
 
 def pear_detect(package):
     """
     Detect if a pear package is installed.
     """
-    if which('pear'):
+    if shell.which('pear'):
         pear_out = local('pear list -a', True)
         if pear_out.find(package) == -1:
             return False
